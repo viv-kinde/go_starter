ebb70debcc437d1edf27359f3a446a885515bfb6
Viv Li <vivian@kinde.com>
2024-02-12 15:53:17 +1100

    update: clean up

--- playground/pages/callback.go
+++ playground/pages/callback.go
@@ -10,30 +10,24 @@ import (
 )
 
 func getAccessToken(w http.ResponseWriter, r *http.Request) string {
-	// Get the authorization code from the query parameters
+
 	code := r.URL.Query().Get("code")
 
-	// If code is empty, check for sessionID
 	if code == "" {
 		sessionID := r.URL.Query().Get("session")
 
 		if sessionID != "" {
 			accessToken := getAccessTokenFromSession(sessionID)
-			fmt.Println("access token from session id: ", accessToken)
 			return accessToken
 		}
 
-		// Handle the case when both code and sessionID are empty
 		fmt.Println("Access token not found")
-		// Handle the error or redirect to login, as needed
-		// http.Redirect(w, r, "/login", http.StatusSeeOther)
-		// return ""
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return ""
 	}
 
-	// Token endpoint URL
 	tokenURL := fmt.Sprintf("%s/oauth2/token", KindeDomain)
 
-	// Build the request payload
 	payload := url.Values{}
 	payload.Set("grant_type", "authorization_code")
 	payload.Set("client_id", KindeClientID)
@@ -41,42 +35,32 @@ func getAccessToken(w http.ResponseWriter, r *http.Request) string {
 	payload.Set("code", code)
 	payload.Set("redirect_uri", LocalDomain+"/dashboard")
 
-	// make POST req to tokenURL endpoint -> send payload
 	res, err := http.Post(tokenURL, "application/x-www-form-urlencoded", strings.NewReader(payload.Encode()))
 	if err != nil {
 		fmt.Println("Error making the request:", err)
-		// return
 	}
 	defer res.Body.Close()
 
-	// read response body
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println("Error reading the response body:", err)
-		// return
 	}
 
-	// check status code for errs
 	if res.StatusCode != http.StatusOK {
 		fmt.Println("Request failed with status:", res.Status)
-		// return
 	}
 
-	// parse response body -> to get access token
 	var response map[string]interface{}
 	err = json.Unmarshal(body, &response)
 	if err != nil {
 		fmt.Println("Error parsing response body:", err)
-		// return
 	}
 
 	accessToken, ok := response["access_token"].(string)
 	if !ok {
 		fmt.Println("Access token not found in the response")
-		// return
 	}
 
-	// If there was a code, generate a session ID and store the access token in the session
 	if code != "" {
 		sessionID := generateSessionID()
 		setAccessTokenInSession(sessionID, accessToken)
@@ -99,11 +83,9 @@ type UserInfo struct {
 }
 
 func getInitials(familyName, givenName string) string {
-	// Extract the first character from each name
 	familyInitial := string(familyName[0])
 	givenInitial := string(givenName[0])
 
-	// Concatenate initials
 	return familyInitial + givenInitial
 }
 
