e8d0bcceca24a9b645d76440871e3f43a0bcac04
Viv Li <vivian@kinde.com>
2024-02-12 15:51:45 +1100

    update: clean up

--- playground/pages/index.go
+++ playground/pages/index.go
@@ -22,6 +22,7 @@ var (
 	LocalDomain       string
 	State             string
 	Code              string
+	RedirectURL       string
 	sessionMutex      sync.Mutex
 
 	sessions = make(map[string]string)
@@ -42,11 +43,9 @@ type TokenResponse struct {
 	RefreshToken string `json:"refresh_token"`
 }
 
-// Function to generate a unique session ID
 func generateSessionID() string {
 	bytes := make([]byte, 32)
 	if _, err := rand.Read(bytes); err != nil {
-		// Handle the error, panic, or return a default value
 		return "default_session_id"
 	}
 	return base64.URLEncoding.EncodeToString(bytes)
@@ -58,14 +57,12 @@ func getAccessTokenFromSession(sessionID string) string {
 	return sessions[sessionID]
 }
 
-// Function to set the access token in the session
 func setAccessTokenInSession(sessionID, accessToken string) {
 	sessionMutex.Lock()
 	defer sessionMutex.Unlock()
 	sessions[sessionID] = accessToken
 }
 
-// takes HTTP response (w) and HTTP request (r) - intended to handle requests to ("/")
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("partials/logged-out.tmpl", "templates/layout.tmpl", "partials/header.tmpl", "partials/footer.tmpl"))
 
@@ -76,8 +73,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func dashboardHandler(w http.ResponseWriter, r *http.Request) {
-	// parses HTML template files
-	// template.Must - panics if there's err during parsing
+
 	tmpl := template.Must(template.ParseFiles("partials/logged-in.tmpl", "templates/layout.tmpl", "partials/header.tmpl", "partials/footer.tmpl"))
 
 	accessToken := getAccessToken(w, r)
@@ -104,23 +100,17 @@ func genRandState() (string, error) {
 }
 
 func readCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
-	// read cookie
 	var cookie, _ = req.Cookie("kindeState")
-	// if err == nil {
-	//     var cookievalue = cookie.Value
-	//     w.Write([]byte(cookievalue))
-	// }
 
 	return cookie
 }
 
 func kindeAuth(w http.ResponseWriter, r *http.Request) {
-	startPage := "login"
+	prompt := "login"
 	if strings.Contains(r.URL.Path, "/register") {
-		startPage = "registration"
+		prompt = "create"
 	}
 
-	// set state in cookie
 	kindeState := &http.Cookie{
 		Name:     "kindeState",
 		Value:    State,
@@ -129,41 +119,17 @@ func kindeAuth(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.SetCookie(w, kindeState)
-	// first cookie ^^
 
 	authURL := fmt.Sprintf("%s/oauth2/auth"+
 		"?response_type=code"+
-		"&start_page=%s"+
+		"&prompt=%s"+
 		"&client_id=%s"+
 		"&redirect_uri=%s/dashboard"+
 		"&scope=openid%%20profile%%20email"+
-		"&state=%s", KindeDomain, startPage, KindeClientID, LocalDomain, State)
+		"&state=%s", KindeDomain, prompt, KindeClientID, LocalDomain, State)
 
 	http.Redirect(w, r, authURL, http.StatusSeeOther)
-
-	// cookieAmount := readCookie(w, r)
-	// accessToken := getAccessToken(w, r)
-	// fmt.Println(accessToken)
 }
-
-func logoutHandler(w http.ResponseWriter, r *http.Request) {
-	cookies := r.Cookies()
-
-	// Iterate through the cookies and set MaxAge to a negative value to delete them
-	for _, cookie := range cookies {
-		cookie.MaxAge = -1
-		http.SetCookie(w, cookie)
-	}
-
-	// http.Redirect(w, r, "/login", http.StatusSeeOther)
-
-}
-
-// Handling the callback
-// send {client_id: 102938, client_secret: 123}
-
-// logout - redirect to kinde url /logout
-// kill anything / storage on their side
 
 func main() {
 	envPath := "../../.env"
@@ -177,25 +143,20 @@ func main() {
 	KindeClientSecret = os.Getenv("KINDE_CLIENT_SECRET")
 	KindeDomain = os.Getenv("KINDE_ISSUER_URL")
 	LocalDomain = os.Getenv("KINDE_SITE_URL")
+	RedirectURL = os.Getenv("KINDE_POST_LOGOUT_REDIRECT_URL")
 
 	State, _ = genRandState()
 
 	http.HandleFunc("/", indexHandler)
 
-	// define user
-	// if user comes back authenticated (isAuthenticated()) - then show:
 	http.HandleFunc("/dashboard", dashboardHandler)
 	http.HandleFunc("/login", kindeAuth)
 	http.HandleFunc("/register", kindeAuth)
 	http.HandleFunc("/logout", logoutHandler)
 
-	// http.HandleFunc("/logout", kindeAuth)
-
-	// Serve CSS files
 	fs := http.FileServer(http.Dir("styles"))
 	http.Handle("/styles/", http.StripPrefix("/styles/", fs))
 
-	// Start the HTTP server
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		panic(err)
 	}
